1f49d5d8d6d112ef871150a25aabef3aca82fd74
Mr-peipei <wushumenmenmen2@gmail.com>
2021-11-07 13:18:56 +0900

    user id

---
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.domain.user.service.impl;
+
+import com.example.domain.user.model.MUser;
+import com.example.domain.user.service.UserService;
+import com.example.repository.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper mapper;
+
+    /**ユーザー登録 */
+    @Override
+    public void signup(MUser user){
+        user.setDepartmentId(1);
+        user.setRole("ROLE_GENERAL");
+        mapper.insertOne(user);
+    }
+
+    @Override
+    public List<MUser> getUsers(){
+        return mapper.findMany();
+    }
+}
---
+++ src/main/java/com/example/repository/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+import com.example.domain.user.model.MUser;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    /*ユーザー登録*/
+    public int insertOne(MUser user);
+
+    /*ユーザー取得*/
+    public List<MUser> findMany();
+}
