2ec98bac35448c0e4ba3e4efa15b2f01a30d5be7
Mr-peipei <wushumenmenmen2@gmail.com>
2021-12-09 21:58:33 +0900

    crate home page

---
+++ src/main/java/com/example/domain/user/service/impl/HomeServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.domain.user.service.impl;
+
+import com.example.domain.user.service.HomeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HomeServiceImpl implements HomeService {
+}
