24c390e23852303f9710c87a6dcdbb9c91da433a
Mr-peipei <wushumenmenmen2@gmail.com>
2021-12-09 22:22:42 +0900

    craet sort

--- src/main/java/com/example/domain/user/service/impl/HomeServiceImpl.java
+++ src/main/java/com/example/domain/user/service/impl/HomeServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.domain.user.model.Tweet;
 import com.example.domain.user.service.HomeService;
 import com.example.repository.TweetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,6 +17,6 @@ public class HomeServiceImpl implements HomeService {
 
     @Override
     public List<Tweet> getAllTweet(){
-        return repository.findAll();
+        return repository.findAll(Sort.by(Sort.Direction.DESC, "TweetKey.tweetDate"));
     }
 }
